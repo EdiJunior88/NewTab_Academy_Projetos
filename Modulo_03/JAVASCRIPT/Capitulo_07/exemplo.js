ccaf85408dad9eb7cc5b1ad53e368f311d2207ae
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-17 13:29:32 -0300

    Sintaxe do JavaScript

---
+++ Modulo_03/JAVASCRIPT/Capitulo_07/exemplo.js
@@ -0,0 +1,17 @@
+console.log('sasasaa', 2, ['sds']);
+
+function nomeFuncao(param) {
+  console.log(param);
+}
+
+funcao = (param) => {
+  console.log(param);
+}
+
+nomeFuncao('teste função');
+funcao('teste 2');
+array1 = ['teste'];
+array2 = [3];
+
+arraycombine = [...array1,array2];
+console.log(arraycombine);
