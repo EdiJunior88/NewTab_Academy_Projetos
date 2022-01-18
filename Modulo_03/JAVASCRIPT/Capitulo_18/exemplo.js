c2febce06dde5a9b936e713ba69dea4dc3310f28
EdiJunior88 <er.moura88@yahoo.com.br>
2022-01-17 22:19:16 -0300

    Loops

---
+++ Modulo_03/JAVASCRIPT/Capitulo_18/exemplo.js
@@ -0,0 +1,74 @@
+// console.log('sasasaa', 2, ['sds']);
+
+// function nomeFuncao(param) {
+//   console.log(param);
+// }
+
+// funcao = (param) => {
+//   console.log(param);
+// }
+
+// nomeFuncao('teste função');
+// funcao('teste 2');
+
+array1 = ['teste'];
+array2 = [3];
+
+arraycombine = [...array1,array2];
+
+arraycombine.forEach(element => {
+  console.log(element);
+});
+
+num = 5
+
+do {
+  num--;
+  console.log(num);  
+} while (num > 3);
+
+num = 5
+for (;num >= -3;) {
+  num--;
+  switch(num) {
+    case 0: 
+      console.log('Zero')
+      break;
+    case 4: 
+      console.log('Quatro')
+      break;
+    default:
+      console.log(num)
+      break;
+  }
+}
+console.log(num);
+
+// console.log(arraycombine);
+
+// tiposdevar = {
+//   integer: 45/0,
+//   float: 65.12,
+//   string: 'Texto',
+//   boolean: true,
+// }
+
+// let variav, teste, teste2 = '';
+
+// Object.values(tiposdevar).map((val) => {
+//   val = val 
+//   console.log(typeof val);
+//   console.log(val);
+// })
+
+// num = 0
+
+// for (i = 0; i < 3; i++) {
+//   console.log(i);
+//   num += 2;
+// }
+
+
+
+// console.log(num);
+
