e7bc6dd8ad2ca4c9e18bc86e7d0fdc3d57afe3ad
EdiJunior88 <er.moura88@yahoo.com.br>
2022-03-03 21:47:29 -0300

    Arrow function

---
+++ Modulo_04/REACT/Capítulo_14/exercicios3/src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () =>
+  <h1>Olá Primeiro Componente2!!!!</h1>
+
+// function primeiro(){
+//   return <h1>Olá Primeiro Componente</h1>
+// }
+
+// export default primeiro
