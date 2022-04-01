0f0c8025362bd486e98f35a64287405a98197d62
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-01 18:22:38 -0300

    Codificação nos editores de texto

---
+++ Modulo_04/REACT/Capitulo_05/app-academy/src/exemplo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+var texto = "Texto exemplo"
+
+export var conteudo = 
+  <div>
+    {texto}
+    
+    <div style={{backgroundColor: "blue"}}>
+      Exemplo 1
+    </div>
+
+    <div className="exemplo">
+      Exemplo 2
+    </div>
+  </div>
