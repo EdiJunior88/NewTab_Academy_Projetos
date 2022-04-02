7758f28b07cfefd0412f0da0aabeccce00321087
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-02 19:31:45 -0300

    Usando componentes e propriedades

---
+++ Modulo_04/REACT/Capitulo_15/exercicios/src/Lista.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const lista = (props) => {
+  return (
+    <div>
+      <ul>
+        <li>Alface</li>
+        <li>Tomate</li>
+        <li>Queijo</li>
+        <li>Macarrão</li>
+        <li>Arroz</li>
+      </ul>
+    </div>
+  )
+};
+
+export default lista;
