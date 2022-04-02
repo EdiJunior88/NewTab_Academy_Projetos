9a538f7808a266aceb584e4fca8136ea47b981bd
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-02 19:59:56 -0300

    Problema com this

---
+++ Modulo_04/REACT/Capitulo_17/exercicios/src/Contador.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+export default class Contador extends Component {
+  
+  maisUm = () => {
+    console.log(this);
+    this.props.numero++
+  }
+  
+  render() {
+    return (
+      <div>
+        <span>Número: {this.props.numero}</span>
+        <button
+          onClick={this.maisUm}
+        >
+          Incremento
+        </button>
+      </div>
+    )
+  }
+}
---
+++ Modulo_04/REACT/Capitulo_17/exercicios/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ComponenteFuncao from './ComponenteFuncao';
+import Contador from "./Contador"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contador />
+    {/* <ComponenteFuncao /> */}
+    {/* <App /> */}
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
