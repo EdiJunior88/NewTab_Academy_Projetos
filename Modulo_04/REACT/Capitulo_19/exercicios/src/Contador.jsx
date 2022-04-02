85c7025b8860296b5c08ad43e81fe0336c4b91d0
EdiJunior88 <er.moura88@yahoo.com.br>
2022-04-02 20:35:20 -0300

    Hooks

---
+++ Modulo_04/REACT/Capitulo_19/exercicios/src/Contador.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+export default class Contador extends Component {
+  
+  state = {
+    numero: this.props.numero
+  }
+
+  maisUm = () => {
+    console.log(this);
+    // this.props.numero++
+    this.setState({numero: this.state.numero + 1})
+  }
+  
+  render() {
+    return (
+      <div>
+        <span>
+          Número: {this.state.numero}
+        </span>
+        
+        <button
+          onClick={this.maisUm}
+        >
+          Incremento
+        </button>
+      </div>
+    )
+  }
+}
