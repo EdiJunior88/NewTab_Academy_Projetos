73aa5b861761bba26dd604f074ba91dbd983d27e
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-16 21:06:06 -0300

    Sintaxe

---
+++ Modulo_07/PHP/Capitulo_05/teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sintaxe</title>
+</head>
+
+<body>
+  <?php
+  echo "Olá Mundo!";
+  ?>
+
+  <br>
+
+  <?php
+  $nomeDaVariavel = "Olá";
+  echo $nomeDaVariavel = "Olá";
+  ?>
+</body>
+
+</html>
