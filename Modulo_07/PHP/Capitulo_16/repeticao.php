5abfbbbcc33499ce39c6de4a7ff11c7198759058
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-17 17:56:02 -0300

    Estrutura de repetição

---
+++ Modulo_07/PHP/Capitulo_16/repeticao.php
@@ -0,0 +1,20 @@
+<?php
+
+// for($i = 1; $i <= 10; $i++) {
+//   if ($i == 2 || $i == 6) {
+//     continue;
+//   }
+
+//   echo $i . "\n";
+// }
+
+$i = 1;
+while($i <= 10) {
+  echo $i++;
+}
+
+// $array = ["Bruno", "Carlos", "Anderson"];
+// // foreach($array as $key => $nome){
+//   for ($i = 0; $i < count($array); $i++) { 
+//   echo $array[$i] . "\n";
+// }
