b7d418b2b95901cfedd07d6f4764a6899e7f91a7
EdiJunior88 <er.moura88@yahoo.com.br>
2022-08-17 16:33:34 -0300

    Operadores

---
+++ Modulo_07/PHP/Capitulo_12/operadores.php
@@ -0,0 +1,28 @@
+<?php
+
+// echo(5**2);
+
+// $soma = 5;
+
+// $soma += 2;
+
+// $soma = $soma + 2;
+
+// $nome= "Bruno";
+
+// $sobrenome = "Benessuti";
+
+// echo $nome . $sobrenome;
+
+// $valorX = True;
+// $valorY = False;
+
+// var_dump ($valorX || $valorY);
+
+$valor = 5;
+
+$valor--;
+
+$valor = $valor - 1;
+
+echo $valor;
